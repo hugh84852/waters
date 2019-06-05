f6b0396add036736a5ea96831e097c43bb66ba12
cindy <cindy5724@hotmail.com>
2019-06-05 13:13:56 +0800

    hi

---
+++ app/src/main/java/com/example/myapplication/activity_addstore.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_addstore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addstore);
+    }
+}
