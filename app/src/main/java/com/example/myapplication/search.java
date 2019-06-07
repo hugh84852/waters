c057ed1ab6729fb19fb4dbd91fafe5d6015b9e0b
ahsuan <ahsuan1027@gmail.com>
2019-06-07 19:50:43 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/myapplication/MainActivity.java
    #	app/src/main/res/layout/activity_search.xml
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/com/example/myapplication/search.java
+++ app/src/main/java/com/example/myapplication/search.java
@@ -61,61 +61,6 @@ public class search extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        List<String> CountysList = new ArrayList<>(Arrays.asList(County));
-
-        // Initializing an ArrayAdapter
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this,R.layout.activity_search_spinner,CountysList){
-            @Override
-            public boolean isEnabled(int position){
-                if(position == 0)
-                {
-                    // Disable the first item from Spinner
-                    // First item will be use for hint
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-            }
-            @Override
-            public View getDropDownView(int position, View convertView, ViewGroup parent) {
-                View view = super.getDropDownView(position, convertView, parent);
-                TextView tv = (TextView) view;
-                if(position == 0){
-                    // Set the hint text color gray
-                    tv.setTextColor(Color.GRAY);
-                }
-                else {
-                    tv.setTextColor(Color.BLACK);
-                }
-                return view;
-            }
-        };
-        adapter1.setDropDownViewResource(R.layout.activity_search_spinner);
-        spinner.setAdapter(adapter1);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedItemText = (String) parent.getItemAtPosition(position);
-                // If user change the default selection
-                // First item is disable and it is used for hint
-                if (position > 0) {
-                    // Notify the selected item text
-                    Toast.makeText
-                            (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
-                            .show();
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
         context = this;
 
         //程式剛啟動始時載入第一個下拉選單
