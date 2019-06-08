5ab0db738e5baeb3db3f30fe76aa61f1ddec5663
cindy <cindy5724@hotmail.com>
2019-06-08 12:44:14 +0800

    hi

--- app/src/main/java/com/example/myapplication/home_1.java
+++ app/src/main/java/com/example/myapplication/home_1.java
@@ -2,9 +2,16 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class home_1 extends AppCompatActivity {
@@ -39,10 +46,13 @@ public class home_1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home_1);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
     }
 
 }
