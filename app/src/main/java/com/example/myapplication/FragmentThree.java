283e51f03798de14022e7551d99dabea691d6f52
peipei857 <kimchi880507@gmail.com>
2019-06-08 15:17:19 +0800

    home_1

---
+++ app/src/main/java/com/example/myapplication/FragmentThree.java
@@ -0,0 +1,24 @@
+
+package com.example.myapplication;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.example.myapplication.R;
+
+
+
+
+public class FragmentThree extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_fragment_three, container, false);
+        return view;
+    }
+
+}
