d71c851d4aa71bb5f1b0afad8b84ecee0a6f3721
peipei857 <kimchi880507@gmail.com>
2019-06-12 17:03:49 +0800

    home_1

--- app/src/main/java/com/example/myapplication/home.java
+++ app/src/main/java/com/example/myapplication/home.java
@@ -1,20 +1,36 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class home extends AppCompatActivity {
+    private ImageButton B1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.content_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        B1 = (ImageButton) findViewById(R.id.imageButton9);
+        B1.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent();
+                intent.setClass(home.this,activity_introduce_store.class);
+                startActivity(intent);
+
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
