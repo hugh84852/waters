78e2101dc802544ba8c075454f6c6e504707ff28
David Shen <whu940033@gmail.com>
2019-06-14 15:23:20 +0800

    連結

--- app/src/main/java/com/example/myapplication/memberinfo.java
+++ app/src/main/java/com/example/myapplication/memberinfo.java
@@ -1,13 +1,63 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class memberinfo extends AppCompatActivity {
+
+    private ImageButton d1;
+    private ImageButton a1;
+    private ImageButton b1;
+    private ImageButton c1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memberinfo);
+
+        d1 = (ImageButton) findViewById(R.id.iB7);
+        d1.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                /* TODO Auto-generated method stub */
+                Intent intent = new Intent();
+                intent.setClass(memberinfo.this,awholenewworld.class);
+                startActivity(intent);
+
+            }
+        });
+        a1 = (ImageButton) findViewById(R.id.iB6);
+        a1.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                /* TODO Auto-generated method stub */
+                Intent intent = new Intent();
+                intent.setClass(memberinfo.this,near_res.class);
+                startActivity(intent);
+
+            }
+        });
+        b1 = (ImageButton) findViewById(R.id.iB9);
+        b1.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                /* TODO Auto-generated method stub */
+                Intent intent = new Intent();
+                intent.setClass(memberinfo.this,story.class);
+                startActivity(intent);
+
+            }
+        });
+        c1 = (ImageButton) findViewById(R.id.iB10);
+        c1.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                /* TODO Auto-generated method stub */
+                Intent intent = new Intent();
+                intent.setClass(memberinfo.this,memberinfo.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
